ad5cf538fc96c5aaa6bc364471b8dcbe5b9428bc
Thanh Nguyen <ngthanh2248@gmail.com>
2023-01-27 15:46:27 -0500

    Add swagger

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const { syncDatabase } = require('./data/util');
 const { sequelize } = require('./models');
 
@@ -16,6 +18,7 @@ const main = async () => {
 	app.use(express.json({ extended: false }));
 
 	app.use('/api/users', require('./routes/users'));
+	app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 	const PORT = process.env.PORT || 8000;
 	app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
