e114024fd7e11908718d998f39f3b9ba7bb19fa0
Thanh Nguyen <ngthanh2248@gmail.com>
2023-01-27 23:04:38 -0500

    Update data for seeding

--- data/index.js
+++ data/index.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const fs = require('fs');
 const { sequelize } = require('../models');
 const { syncDatabase } = require('./util');
-const { createMovies } = require('../services/movieService');
-const { createGenres } = require('../services/genreService');
+const { createMovies, createMovie } = require('../services/movieService');
+const { createGenres, getGenreByName } = require('../services/genreService');
 
 sequelize
 	.authenticate()
@@ -12,7 +12,15 @@ sequelize
 		var genres = JSON.parse(fs.readFileSync('./data/genres.json', 'utf8'));
 		var movies = JSON.parse(fs.readFileSync('./data/movies.json', 'utf8'));
 		await createGenres(genres);
-		await createMovies(movies);
+		// much slower than before, but well :v
+		for (const movie of movies) {
+			const createdMovie = await createMovie(movie);
+			for (const genre of movie.genres) {
+				const foundGenre = await getGenreByName(genre);
+				await createdMovie.addGenre(foundGenre.uuid);
+			}
+		}
+		// await createMovies(movies);
 		console.log('Populated database');
 	})
 	.catch((error) => {
