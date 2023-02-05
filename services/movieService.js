54f527fad8a66401ac5f72026a2e9363340d776a
Thanh Nguyen <ngthanh2248@gmail.com>
2023-02-04 23:29:50 -0500

    Add endpoints for getting popular movies

--- services/movieService.js
+++ services/movieService.js
@@ -1,6 +1,18 @@
 const StatusError = require('../errors');
 const { Genre } = require('../models');
 const movieRepo = require('../repositories/movieRepo');
+const { Op } = require('sequelize');
+
+const LIMIT = 19;
+
+const getMoviesQuery = (movies, limit) => {
+	let hasMore = false;
+	if (movies.length === limit) {
+		movies.pop();
+		hasMore = true;
+	}
+	return { movies, hasMore };
+};
 
 const getMovieById = async (uuid) => {
 	const movie = await movieRepo.findOne({ where: { uuid } });
@@ -10,12 +22,53 @@ const getMovieById = async (uuid) => {
 	return movie;
 };
 
-const getAllMovies = () => {
-	return movieRepo.findAll({
-		include: {
-			model: Genre,
-		},
-	});
+/**
+ * @param query req.query
+ */
+const getPopularMovies = async (query) => {
+	const { sort, limit, popularity, uuid } = query;
+	const moviesLimit = limit ? parseInt(limit, 10) : LIMIT;
+	const sortBy = sort === 'ascending' ? 'ASC' : 'DESC';
+	try {
+		const movies = await movieRepo.findAll({
+			where: popularity
+				? {
+						[Op.or]: [
+							{
+								popularity: {
+									[Op.lt]: popularity,
+								},
+							},
+							{
+								[Op.and]: [
+									{
+										popularity: {
+											[Op.eq]: popularity,
+										},
+									},
+									{
+										uuid: {
+											[Op.lt]: uuid,
+										},
+									},
+								],
+							},
+						],
+				  }
+				: null,
+			include: {
+				model: Genre,
+			},
+			order: [
+				['popularity', sortBy],
+				['uuid', sortBy],
+			],
+			limit: moviesLimit,
+		});
+		return getMoviesQuery(movies, moviesLimit);
+	} catch (error) {
+		throw new StatusError([{ msg: error.message }], 500);
+	}
 };
 
 const createMovie = (movie) => {
@@ -28,7 +81,7 @@ const createMovies = (movies) => {
 
 module.exports = {
 	getMovieById,
-	getAllMovies,
+	getPopularMovies,
 	createMovie,
 	createMovies,
 };
